ad0bd73617b685ebb4106216bb8b466cab0a7ea9
Mr.Aurora <3772589225@qq.com>
2026-02-20 00:00:17 +0800

    face_detect_node finished

---
+++ Chapter_four/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.node import Node
+from chapt4_interfaces.srv import FaceDetector
+import face_recognition
+import cv2
+import os
+from ament_index_python.packages import get_package_share_directory
+from cv_bridge import CvBridge
+import time
+
+class FaceDetectNode(Node):
+    def __init__(self):
+        super().__init__('face_detect_node')
+        self.service_ = self.create_service(FaceDetector, 'face_detect', self.detect_face_callback)
+        self.bridge = CvBridge()
+        self.number_of_times_to_upsample = 1
+        self.model = 'hog'
+        self.default_image_path = os.path.join(get_package_share_directory('demo_python_service'), 'source/default.jpg')
+        self.get_logger().info("人脸检测服务以启动！")
+
+    def detect_face_callback(self, request, response):
+        if request.image.data:
+            cv_image = self.bridge.imgmsg_to_cv2(request.image)
+        else:
+            cv_image = cv2.imraed(self.default_image_path)
+            self.get_logger().info(f"传入图像为空，使用默认图像！")
+        # cv_image 已经是一个opencv格式的图像了
+
+        start_time = time.time()
+        self.get_logger().info(f"加载完成图像，开始识别！")
+
+        # 检测人脸
+        face_locations = face_recognition.face_locations(cv_image, number_of_times_to_upsample=self.number_of_times_to_upsample, model=self.modle)
+        response.use_time = time.time() - start_time
+        response.number = len(face_locations)
+        for top, right, bottom, left in face_locations:
+            response.top.append(top)
+            response.right.append(right)
+            response.bottom.append(bottom)
+            response.left.append(left)
+
+        return response 
+
+def main():
+    rclpy.init()
+    node = FaceDetectNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
